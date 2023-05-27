9f49f28b08105015695a1357290d23b1174b58c5
Shriamrut V <vshriamrut@vmware.com>
2023-05-27 18:00:51 +0530

    Add git ignore and color options

--- bard-cli.py
+++ bard-cli.py
@@ -2,6 +2,14 @@ from bard import AskBard
 import argparse
 API_TOKEN = '<TOKEN>'
 
+
+class bcolors:
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "Parse arguments")
     parser.add_argument('-p', '--prompt')
@@ -9,10 +17,10 @@ if __name__ == "__main__":
     bard = AskBard(api_key = API_TOKEN)
     if args.prompt:
         response = bard.ask(args.prompt)
-        print("Bard: ", response, end = "\n\n")
+        print(bcolors.BOLD + bcolors.OKCYAN + "Bard: " + bcolors.ENDC, response, end = "\n\n")
     while True:
-        prompt = input("Enter a prompt here: ")
+        prompt = input(bcolors.BOLD + bcolors.OKGREEN + "Enter a prompt here: " + bcolors.ENDC)
         if prompt == "quit":
             break
         response = bard.ask(prompt)
-        print("Bard: ", response, end = "\n\n")
+        print(bcolors.BOLD + bcolors.OKCYAN + "Bard: ", response, end = "\n\n")
